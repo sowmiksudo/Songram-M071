42df68c88ee8ed8b3924f9545411b34a08af4d46
Shayer Mahmud Sowmik <69670910+sowmiksudo@users.noreply.github.com>
2023-10-24 21:51:46 +0600

    added muz bro codes

---
+++ muztahid-vai-codes/config.h
@@ -0,0 +1,24 @@
+#define IO_USERNAME "enconiya"
+#define IO_KEY "aio_PIET07LploJVjhflbyzXAe8PF4KU"
+
+
+#define WIFI_SSID "Adsec Education _Guest"
+#define WIFI_PASS "adsecguest"
+#include "AdafruitIO_WiFi.h"
+
+#if defined(USE_AIRLIFT) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE) ||         \
+    defined(ADAFRUIT_PYPORTAL)
+
+#if !defined(SPIWIFI_SS) // if the wifi definition isnt in the board variant
+
+#define SPIWIFI SPI
+#define SPIWIFI_SS 10  // Chip select pin
+#define SPIWIFI_ACK 9  // a.k.a BUSY or READY pin
+#define ESP32_RESETN 6 // Reset pin
+#define ESP32_GPIO0 -1 // Not connected
+#endif
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS, SPIWIFI_SS,
+                   SPIWIFI_ACK, ESP32_RESETN, ESP32_GPIO0, &SPIWIFI);
+#else
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+#endif
