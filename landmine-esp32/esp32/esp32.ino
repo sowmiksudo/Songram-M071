49ad59b8a23dad6cfb3447ccea37b2cac0485a62
Shayer Mahmud Sowmik <69670910+sowmiksudo@users.noreply.github.com>
2023-10-26 00:22:03 +0600

    SPI mode success

--- landmine-esp32/esp32/esp32.ino
+++ landmine-esp32/esp32/esp32.ino
@@ -1,107 +1,50 @@
-// esp32-wroom-32 devkit v1 default pins
-//       SCL D22
-//       SDA D21
-
-// Define the interface type
-#if 0
 #include <SPI.h>
-#include <PN532_SPI.h>
-#include "PN532.h"
-PN532_SPI pn532spi(SPI, 10);
-PN532 nfc(pn532spi);
+#include <Adafruit_PN532.h>
+#define PN532_SCK (18)
+#define PN532_MISO (19)
+#define PN532_MOSI (23)
+#define PN532_SS (27)
 
-#elif 0
-#include <PN532_HSU.h>
-#include <PN532.h>
-PN532_HSU pn532hsu(Serial1);
-PN532 nfc(pn532hsu);
+Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
 
-#else
-#include <Wire.h>
-#include <PN532_I2C.h>
-#include <PN532.h>
-#include <NfcAdapter.h>
+String idcard;
 
-PN532_I2C pn532i2c(Wire);
-PN532 nfc(pn532i2c);
-#endif
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  nfc.begin();
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (!versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1)
+      ;  // halt
+  }
+  Serial.print("Found chip PN5");
+  Serial.println((versiondata >> 24) & 0xFF, HEX);
+  Serial.print("Firmware ver. ");
+  Serial.print((versiondata >> 16) & 0xFF, DEC);
+  Serial.print('.');
+  Serial.println((versiondata >> 8) & 0xFF, DEC);
 
-volatile bool connected = false;
-
-void setup(void)
-{
-  Serial.begin(115200);
-  Serial.println("*** Testing Module PN532 NFC RFID ***");
+  nfc.SAMConfig();
 }
 
-void loop(void)
-{
-  boolean success;
-  // Buffer to store the UID
-  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };
-  // UID size (4 or 7 bytes depending on card type)
+void loop() {
+  // put your main code here, to run repeatedly:
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
   uint8_t uidLength;
 
-  while (!connected) {
-    connected = connect();
-  }
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
 
-  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
-  // 'uid' will be populated with the UID, and uidLength will indicate
-  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
-
-  // If the card is detected, print the UID
-  if (success)
-  {
-    Serial.println("Card Detected");
-    Serial.print("Size of UID: "); Serial.print(uidLength, DEC);
-    Serial.println(" bytes");
-    Serial.print("UID: ");
-    for (uint8_t i = 0; i < uidLength; i++)
-    {
-      Serial.print(" 0x"); Serial.print(uid[i], HEX);
+  if (success) {
+    // Display some basic information about the card
+    idcard = "";
+    for (byte i = 0; i <= uidLength - 1; i++) {
+      idcard += (uid[i] < 0x10 ? "0" : "") + String(uid[i], HEX);
     }
+    Serial.print("ID CARD : ");
+    Serial.print(idcard);
     Serial.println("");
-    Serial.println("");
-    
-    delay(1000);
-    connected = connect();
   }
-  else
-  {
-    // PN532 probably timed out waiting for a card
-    // Serial.println("Timed out waiting for a card");
-  }
-}
-
-bool connect() {
-  
-  nfc.begin();
-
-  // Connected, show version
-  uint32_t versiondata = nfc.getFirmwareVersion();
-  if (! versiondata)
-  {
-    Serial.println("PN53x card not found!");
-    return false;
-  }
-
-  //port
-  Serial.print("Found chip PN5"); Serial.println((versiondata >> 24) & 0xFF, HEX);
-  Serial.print("Firmware version: "); Serial.print((versiondata >> 16) & 0xFF, DEC);
-  Serial.print('.'); Serial.println((versiondata >> 8) & 0xFF, DEC);
-
-  // Set the max number of retry attempts to read from a card
-  // This prevents us from waiting forever for a card, which is
-  // the default behaviour of the PN532.
-  nfc.setPassiveActivationRetries(0xFF);
-
-  // configure board to read RFID tags
-  nfc.SAMConfig();
-
-  Serial.println("Waiting for card (ISO14443A Mifare)...");
-  Serial.println("");
-
-  return true;
 }
